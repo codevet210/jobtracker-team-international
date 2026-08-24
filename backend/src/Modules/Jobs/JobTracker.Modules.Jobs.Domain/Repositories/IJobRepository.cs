2cf9dd87aa664ff54e4d5c45fce19b73906daaea
codevet210 <sharp.blader2050@gmail.com>
2026-08-24 09:52:34 -0300

    add job repository

---
+++ backend/src/Modules/Jobs/JobTracker.Modules.Jobs.Domain/Repositories/IJobRepository.cs
@@ -0,0 +1,14 @@
+using JobTracker.Modules.Jobs.Domain.Jobs;
+
+namespace JobTracker.Modules.Jobs.Domain.Repositories;
+
+public interface IJobRepository
+{
+    Task<Job?> GetByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    Task AddAsync(
+        Job job,
+        CancellationToken cancellationToken = default);
+}
